b59d46de5bd30cffa153a45298f2a794bcf66585
Madhan Raj Mookkandy <madhanm@microsoft.com>
2020-03-19 16:40:24 -0700

    Build

--- pkg/virtualization/core/virtualsystemmanagement/virtualmachinemanagementservice.go
+++ pkg/virtualization/core/virtualsystemmanagement/virtualmachinemanagementservice.go
@@ -1,12 +1,13 @@
 package virtualmachinemanagementservice
 
 import (
-	wmi "github.com/microsoft/wmi/pkg/wmiinstance"
-	wmivm "github.com/microsoft/wmi/server2019/root/virtualization/v2"
+	"github.com/microsoft/wmi/server2019/root/virtualization/v2"
 )
 
-func CreateVirtualMachine() (vm *wmivm.Msvm_ComputeSystem, err error) {
+func CreateVirtualMachine() (vm *v2.Msvm_ComputerSystem, err error) {
+	return nil, nil
 }
 
 func GetVirtualMachine(hostname, vmName string) {
+	return
 }
--- pkg/virtualization/network/virtualswitchservicestore.go
+++ pkg/virtualization/network/virtualswitchservicestore.go
@@ -4,10 +4,6 @@
 package network
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
 	"github.com/microsoft/wmi/pkg/base/host"
 	wmi "github.com/microsoft/wmi/pkg/wmiinstance"
 )
