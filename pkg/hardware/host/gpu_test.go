3b0fa152cba837d80a1e85c0d59b5149da79f8a0
Dhaval Popat <dhavalpopat10@gmail.com>
2024-04-10 16:48:08 -0700

    Get required data for host GPU-P availability check

---
+++ pkg/hardware/host/gpu_test.go
@@ -0,0 +1,19 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+package host
+
+import (
+	wmihost "github.com/microsoft/wmi/pkg/base/host"
+	_ "github.com/microsoft/wmi/pkg/base/session"
+	"testing"
+)
+
+func TestGetPartitionableGpuCollection(t *testing.T) {
+	whost := wmihost.NewWmiLocalHost()
+	gpuCollection, err := GetPartitionableGpuCollection(whost)
+	if err != nil {
+		t.Fatalf("[%+v]", err)
+	}
+
+	t.Logf("Partitionable GPUs [%+v]\n", gpuCollection)
+}
